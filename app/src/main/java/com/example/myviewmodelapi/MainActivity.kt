91478fc53cf0e1cd39aa4e16cc4e53779426bfcb
Nuril Muslichin <nurilmuslichin16@gmail.com>
2020-10-21 21:15:53 +0700

    View Model

---
+++ app/src/main/java/com/example/myviewmodelapi/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.myviewmodelapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.lang.Exception
+import java.text.DecimalFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: WeatherAdapter
+    private lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        adapter = WeatherAdapter()
+        adapter.notifyDataSetChanged()
+
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.adapter = adapter
+
+        mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+
+        btn_city.setOnClickListener{
+            val city = edt_city.text.toString()
+            if (city.isEmpty()) return@setOnClickListener
+            showLoading(true)
+
+            mainViewModel.setWeather(city)
+        }
+
+        mainViewModel.getWeather().observe(this, Observer { weatherItems ->
+            if (weatherItems != null) {
+                adapter.setData(weatherItems)
+                showLoading(false)
+            }
+        })
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            progress_bar.visibility = View.VISIBLE
+        } else {
+            progress_bar.visibility = View.GONE
+        }
+    }
+}
